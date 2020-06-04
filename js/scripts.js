a1e3ea9eb8715fbcabde93e0a627454cd6c3f409
Tristan Emmerson <tristan@stickerslug.com>
2020-06-04 09:51:17 -0700

    Add age verification prompt for user when entering site.
    co-authored by: Micheal Hansen <micheal.hansen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
-$("form#born").submit(function(event) {
-  const someInput = $("input#born").val();
-  event.preventDefault();
-  const dob = $("#born").val();
-});
+var yourAge = prompt("Please enter your age: ")
+
+if (yourAge >= 18) {
+    alert("Vote here!");
+    document.getElementById("content").style.display = "block";
+} else {
+    alert("You are not legally of age yet. Here are some resources though");
+    location.href = ("../html/index.html");
+}
